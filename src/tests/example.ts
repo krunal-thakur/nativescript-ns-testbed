fb83c041bf71a43e1262feaed529d8ea14b07672
George Savchenko <george.savchenko@gmail.com>
2019-07-24 14:06:16 -0400

    removing item-service

--- src/tests/example.ts
+++ src/tests/example.ts
@@ -1,12 +1,10 @@
-import { ItemService } from "../app/item/item.service";
 import { ItemsComponent } from '~/app/item/items.component';
 import { By } from '@angular/platform-browser';
 import { nsTestBedBeforeEach, nsTestBedAfterEach, nsTestBedRender } from 'nativescript-angular/testing';
 
 describe('item-detail-component', () => {
   beforeEach(nsTestBedBeforeEach(
-    [ItemsComponent],
-    [ItemService]
+    [ItemsComponent]
   ));
   afterEach(nsTestBedAfterEach());
 
